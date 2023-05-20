3489822d15371eb68d29ada8123df4222ec9057e
Jennifer <joonnifer@gmail.com>
2023-05-20 16:07:19 -0700

    add route

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 const { Schema } = mongoose;
 
+app.use(cors())
+app.use(express.json())
 mongoose.connect(process.env.DB_URL)
 
 const UserSchema = new Schema ({
@@ -20,8 +22,6 @@ const ExerciseSchema = new Schema ({
 });
 const Exercise = mongoose.model("Exercise", ExerciseSchema)
 
-
-app.use(cors())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 
@@ -29,21 +29,49 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-app.post('/api/users', async (req, res) => {
-  console.log(req.body)
-  
+app.post('/api/users', async (req, res) => {  
   const userObject = new User({
     username: req.body.username
   })
 
   try {
     const user = await userObject.save()
-    console.log(user)
+    console.log('try adding user', user)
     res.json( user )
   } catch(err) {
     console.log(err)
   }
 
+})
+
+app.post('/api/users/:_id/exercises', async (req, res) => {  
+  const id = req.params._id
+  const { description, duration, date } = req.body
+
+  try {
+    const user = await User.findById(id)
+    
+    if( !user ) {
+      res.send("User not found")
+    }
+
+    const exerciseObject = new Exercise({
+      user_id: id,
+      description,
+      duration,
+      date: date? new Date(date) : new Date()
+    })
+    const exercise = exerciseObject.save()
+    res.json({
+      _id: user._id,
+      username: user.username,
+      description: exercise.description,
+      date: new Date(exercise.date).toDateString()
+    })
+
+  } catch(err) {
+    console.log(err)
+  }
 })
 
 const listener = app.listen(process.env.PORT || 3000, () => {
